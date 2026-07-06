814a23a5608582af4ad4d36e9966c7e43b886cae
marisparv-debug <marisparv@gmail.com>
2026-07-07 00:34:18 +0200

    added JavaScript layer to the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("Welcome to the norwegian poems generator!");
+// alert("Welcome to the norwegian poems generator!");
+function generatePoem(event) {
+  event.preventDefault();
+  let poemElement = document.querySelector("#poem");
+  new Typewriter("#poem", {
+    strings: "Poem genereres right now...",
+    autoStart: true,
+    delay: 2,
+    cursor: "",
+  });
+}
+
+let poemFormElemet = document.querySelector("#poem-generator-form");
+poemFormElemet.addEventListener("submit", generatePoem);
